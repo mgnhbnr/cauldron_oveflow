c0ca8ec25dd8043fd776c290aea282a8ef71ac0a
Megan <megan@MacBook-Air-von-Megan.local>
2024-01-17 14:44:01 +0100

    using controller to render page

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,28 +2,34 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
      */
     public function homepage() {
-        return new Response('test welcome');
+        return new Response('welcome');
     }
 
     /**
      * @Route("/questions/{slug}")
      */
     public function show($slug) {
-        return new Response(sprintf(
-            'Future page to show the question "%s"!',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+
+        $answers = [
+            "But, sir, it's an emergency.",
+            "Come back when it's a catastrophe."
+        ];
+
+        return $this -> render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 
 }
 
-// ucwords turns every first letter within a string into Uppercase
