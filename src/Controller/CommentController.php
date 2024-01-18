d89a3b1e381c7c959f1ba33810c5793590ffe542
mgnhbnr <mh@creavo.de>
2024-01-18 12:06:13 +0100

    added logger output

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,19 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction}")
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction) {
+    public function commentVote($id, $direction, LoggerInterface $logger) {
 
         if($direction === "up") {
+            $logger ->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         }
         else {
+            $logger ->info('Voting down!');
             $currentVoteCount = rand(0, 5);
         }
 
         return $this -> json(['votes' => $currentVoteCount]);
-//      alternative:  return new JsonResponse(['votes' => $currentVoteCount]);
+        // alternative:  return new JsonResponse(['votes' => $currentVoteCount]);
 
     }
 }
